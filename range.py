28346fd90e8f934b5d84921af3cbc41b53d4c506
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-12-22 12:14:51 +0100

    funguje potreba jine piny

--- range.py
+++ range.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=22, trigger=27)
+sensor = DistanceSensor(echo=24, trigger=23, max_distance=2)
 while True:
     print(sensor.distance*100)
     sleep(1)
