9f8e6963b96911e7e4128075995d1d20ffd25409
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-12-22 12:38:06 +0100

    alojz

---
+++ alojz.py
@@ -0,0 +1,23 @@
+from gpiozero import DistanceSensor
+from signal import pause
+from os import system
+from time import sleep
+
+run = 0.7
+
+def on():
+    system("/home/pi/bin/on")
+    sleep(10)
+    if sensor.distance > run:
+        system("/home/pi/bin/off")
+
+def off():
+    system("/home/pi/bin/off")
+
+
+sensor = DistanceSensor(echo=24, trigger=23, max_distance=2, threshold_distance=run, queue_len=5)
+
+sensor.when_in_range = on
+sensor.when_out_of_range =  off
+
+pause()
