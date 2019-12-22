d5ba16b0cfb598a8b8e3460a9358e7ca35cc901a
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-12-22 15:32:02 +0100

    uprava alarmu

--- alojz.py
+++ alojz.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 from gpiozero import DistanceSensor
-from signal import pause
 from os import system
+import signal
 import time
 from time import sleep
-
-run = 0.7
 
 def on():
     system("/home/pi/bin/on")
@@ -16,20 +14,31 @@ def on():
 def off():
     system("/home/pi/bin/off")
 
-sensor = DistanceSensor(echo=24, trigger=23, max_distance=2, threshold_distance=run, queue_len=5)
+def Run():
+    while True:
+        t = time.asctime(time.localtime(time.time())).split(' ')[3]
+        if t >= '22:00:00' or t < '07:00:00':
+            off()
+            sensor.when_in_range = None
+            sensor.when_out_of_range = None
+            sleep(1)
+        elif t >= '07:00:00' and t <= '09:00:00':
+            on()
+            sensor.when_in_range = None
+            sensor.when_out_of_range = None
+            sleep(1)
+        else:
+            off()
+            sensor.when_in_range = on
+            sensor.when_out_of_range =  off
+            s = time.asctime(time.localtime(time.time())).split(' ')
+            s[3] = '22:00:00'
+            stop = time.mktime(time.strptime(' '.join(s)))
+            delta = stop-time.time()
+            signal.alarm(int(delta))
+            signal.pause()
 
-while True:
-    t = time.asctime(time.localtime(time.time())).split(' ')[3]
-    if t >= '22:00:00' or t < '07:00:00':
-        off()
-        sensor.when_in_range = None
-        sensor.when_out_of_range = None
-    elif t >= '07:00:00' and t <= '09:00:00':
-        on()
-        sensor.when_in_range = None
-        sensor.when_out_of_range = None
-    else:
-        off()
-        sensor.when_in_range = on
-        sensor.when_out_of_range =  off
-    sleep(1)
+run = 0.7
+sensor = DistanceSensor(echo=24, trigger=23, max_distance=2, threshold_distance=run, queue_len=5)
+signal.signal(signal.SIGALRM, Run)
+Run()
