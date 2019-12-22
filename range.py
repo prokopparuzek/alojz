1b0c472a943864a351f4df45b64f1033c1288afb
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-12-22 12:26:00 +0100

    queue_len

--- range.py
+++ range.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=24, trigger=23, max_distance=2)
+sensor = DistanceSensor(echo=24, trigger=23, max_distance=2, queue_len=5)
 while True:
     print(sensor.distance*100)
     sleep(1)
