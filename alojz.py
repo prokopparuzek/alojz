a6563129768ce9236cc3238dedf6f5b1907a5a74
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-01-13 18:02:08 +0100

    vnejsi zdroj hodin, cron

--- alojz.py
+++ alojz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from gpiozero import DistanceSensor
 from os import system
+import os
 import signal
 import time
 from time import sleep
@@ -12,33 +13,24 @@ def on():
 def off():
     system("/home/pi/bin/off")
 
-def Wake(when, plus):
-    '''vzbud mne'''
-    s = time.asctime(time.localtime(time.time() + plus)).split(' ')
-    s[3] = when
-    stop = time.mktime(time.strptime(' '.join(s)))
-    delta = stop-time.time()
-    signal.alarm(int(delta))
-    signal.pause()
-
 def Run(signum, frame): #handler musí příjmat 2 argumenty (číslo signálu a frame (odkudse volá))
     '''switch dle casu'''
+    with open("/tmp/alojz.pid", "w", encoding="UTF-8") as f:
+        f.write(str(os.getpid()))
     t = time.asctime(time.localtime(time.time())).split(' ')[3]
     if t >= '22:00:00' or t < '07:00:00':
         off()
         sensor.when_in_range = None
         sensor.when_out_of_range = None
-        Wake('07:00:00', 36000)
     elif t >= '07:00:00' and t <= '09:00:00':
         on()
         sensor.when_in_range = None
         sensor.when_out_of_range = None
-        Wake('09:00:00', 0)
     else:
         off()
         sensor.when_in_range = on
         sensor.when_out_of_range =  off
-        Wake('22:00:00', 0)
+    signal.pause()
 
 run = 0.7
 sensor = DistanceSensor(echo=24, trigger=23, max_distance=2, threshold_distance=run, queue_len=5)
